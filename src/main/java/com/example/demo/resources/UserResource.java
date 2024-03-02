965e7e42727c7a29b94fe470138dd88ef6fca449
Edson Costa de Siqueira Neto <edsonsiqueiraneto@gmail.com>
2024-03-02 19:35:09 -0300

    Package service and annotation component

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.example.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService userService;
 
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Edson", "edson@gmail.com", "999999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		//resource/controller acessa o repository apenas por meio do service
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	//a anotação pathVariable serve para indicar que o metodo pegue o argumento que vai vir na url
+	public ResponseEntity<User> findByEntity(@PathVariable Long id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
